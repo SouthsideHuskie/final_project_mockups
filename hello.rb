343711d10ae6778aa5904b9b54a3dd31a8b8455c
Ryan McGuinness <ryanmmcguinness@gmail.com>
2014-04-14 19:09:02 -0500

    Ruby

---
+++ hello.rb
@@ -0,0 +1,21 @@
+# def first_person
+#   first="raghu"
+#   last="betina"
+#   full=first.capitalize+" "+last.capitalize
+
+#   puts full
+
+# end
+
+# first_person
+
+def full_name_for
+  first="raghu"
+  last="betina"
+  full=first.capitalize+" "+last.capitalize
+
+  puts full
+
+end
+
+full_name_for
